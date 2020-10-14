9d671879735e9caf14714aada9c5e753d5a79042
Paul Miller <paul@pauljmiller.com>
2020-10-14 13:19:08 -0400

    kraken

---
+++ components/ResumeLayout.tsx
@@ -0,0 +1,150 @@
+import Head from "next/head";
+import { Window } from "../components/Window";
+import { Nav } from "../components/Nav";
+
+const Layout = ({ children, pageTitle, windowTitle, navActive }) => {
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <script async src="https://btcpay.paul.lol/modal/btcpay.js"></script>
+      </Head>
+      <div>
+        <main className="column">
+          <div className="windows">
+            <Window title={windowTitle}>{children}</Window>
+            <Window title="INFO">
+              <h2 id="info">Info</h2>
+              <ul>
+                <li>Brooklyn, NY</li>
+                <li>509 845-1595</li>
+                <li>
+                  <a href="mailto:paul@paul.lol">paul@paul.lol</a>
+                </li>
+                <li>
+                  <a
+                    href="
+paul@paul.lol"
+                  >
+                    @futurepaul
+                  </a>
+                </li>
+                <li>
+                  <a href="https://pauljmiller.com/projects.html">
+                    pauljmiller.com/projects.html
+                  </a>
+                </li>
+              </ul>
+              <h2 id="skills">Skills</h2>
+              <ul>
+                <li>React / React Native</li>
+                <li>JavaScript / TypeScript</li>
+                <li>Rust</li>
+                <li>Figma</li>
+                <li>Git / GitHub</li>
+                <li>HTML / CSS / Flexbox</li>
+              </ul>
+              <h2 id="education">Education</h2>
+              <ul>
+                <li>Code reviews</li>
+                <li>Udemy</li>
+                <li>YouTube</li>
+                <li>Buidl Bootcamp</li>
+              </ul>
+            </Window>
+          </div>
+        </main>
+        <style jsx>{`
+          .logo img {
+            width: 317px;
+          }
+          .column {
+            display: flex;
+            flex-direction: column;
+          }
+
+          .windows {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: start;
+            align-content: stretch;
+          }
+
+          main {
+            justify-content: center;
+            align-items: center;
+          }
+          p,
+          h3,
+          li {
+            font-size: 14px;
+            font-family: IBM Plex Serif;
+            font-style: normal;
+            font-weight: 300;
+            line-height: 110%;
+
+            /* or 18px */
+            letter-spacing: -0.05em;
+          }
+
+          ul {
+            list-style: none;
+            padding-left: 0;
+          }
+
+          li {
+            padding-left: 1em;
+            padding-bottom: 0.1em;
+          }
+
+          h2 {
+            font-family: Inter;
+            font-style: italic;
+            font-weight: 1000;
+            font-size: 1.5rem;
+            margin: 0.25rem;
+            border-bottom: 1px dotted black;
+          }
+
+          h1 {
+            color: white;
+            font-size: 96px;
+            font-weight: 1000;
+            font-style: italic;
+            margin: 0;
+            margin-top: 1rem;
+          }
+
+          .alt-logo {
+            transform: scale(0);
+          }
+          a {
+            color: blue;
+          }
+        `}</style>
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
+              Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+              Helvetica Neue, sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ pages/resume.tsx
@@ -0,0 +1,236 @@
+import ResumeLayout from "../components/ResumeLayout";
+import React, { useState } from "react";
+
+const Pay = () => {
+  return (
+    <ResumeLayout pageTitle="paul.lol" windowTitle="RESUME" navActive="PAY">
+      <main>
+        <article>
+          <h1 id="who-is-paul-miller">Who is Paul?</h1>
+          <section>
+            <p>
+              Paul Miller is a mobile app developer with half a year of
+              professional experience building React Native apps and designing
+              UIs in Figma. Also: over a year of Rust desktop GUI open source
+              work. Great at finding hairy problems and doing the
+              &quot;dumb&quot; thing to get the conversation moving toward the
+              right solution. Recently reskilled from writer to developer.
+            </p>
+          </section>
+          <h1 id="experience">Experience</h1>
+          <section>
+            <h2 id="mobile-app-developer">Mobile App Developer</h2>
+            <p>
+              <em>Giant Killer - Sunbury, OH (Remote) / May 2020 - Present</em>
+            </p>
+            <ul>
+              <li>
+                Worked on near total rewrite of primary app to TypeScript and
+                more idiomatic React, resulting in an almost 50 percent
+                reduction code base size while also reducing bugs, adding
+                features, and adding some new bugs yet to be found.
+              </li>
+              <li>
+                Designed (in Figma) and implemented a new &quot;Message
+                Center&quot; portion of the app which added attachments and
+                photos to the existing text messaging of the app.
+              </li>
+              <li>
+                Troubleshooting of hairy React Native problems often caused by
+                poorly maintained dependencies.
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2 id="open-source-contributor">Open Source Contributor</h2>
+            <p>
+              <em>Druid - Github, The Internet / May 2019 - Present</em>
+            </p>
+            <ul>
+              <li>
+                While learning Rust I became a top contributer to the Druid
+                desktop GUI project.
+              </li>
+              <li>
+                Did the first pass at an almost-full-featured single line
+                editable text box.
+              </li>
+              <li>
+                Designed the default look for Druid in Figma which nobody hates
+                enough to change yet.
+              </li>
+              <li>
+                Implemented some basic widgets like Slider, Progress Bar, and
+                Svg.
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2 id="technology-writer">Technology Writer</h2>
+            <p>
+              <em>The Verge - New York, NY / 2011 - 2020 on and off</em>
+            </p>
+            <ul>
+              <li>
+                Helped found <em>The Verge</em>, a leading publication in the
+                space.
+              </li>
+              <li>
+                Wrote and edited everything from news to reviews to opinion.
+              </li>
+              <li>
+                Co-hosted <em>The Vergecast</em> podcast.
+              </li>
+            </ul>
+          </section>
+        </article>
+      </main>
+
+      <style jsx>{`
+        main {
+          width: 25rem;
+        }
+
+        .prose {
+          display: flex;
+        }
+
+        input[type="text"] {
+          border: 1px solid black;
+          padding: calc(0.5rem - 1px);
+          margin-top: 1px;
+          margin-bottom: 1rem;
+          font-family: IBM Plex Serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 110%;
+        }
+        .button {
+          color: white;
+          background-color: black;
+          padding: 0.5rem;
+          font-family: IBM Plex Serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 110%;
+          border: 1px solid white;
+        }
+        section {
+          padding: 0.2rem;
+          width: 100%;
+          margin-left: 0em;
+          margin-right: auto;
+        }
+        .prompt {
+          font-family: monospace;
+          font-weight: bold;
+          color: rgba(179, 255, 102, 230);
+          position: relative;
+          margin-left: 60px;
+          margin-top: 60px;
+        }
+        .subscribe {
+          margin-top: 2rem;
+        }
+        main {
+          margin: auto;
+        }
+        article {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        em {
+          text-decoration: underline;
+        }
+
+        .responsive {
+          max-width: 100%;
+          height: auto;
+        }
+
+        .column {
+          display: flex;
+          flex-direction: column;
+          width: 15rem;
+          margin: 1rem;
+          align-items: center;
+          justify-content: center;
+        }
+        p,
+        h3,
+        li {
+          font-size: 14px;
+          font-family: IBM Plex Serif;
+          font-style: normal;
+          font-weight: 300;
+          line-height: 110%;
+
+          /* or 18px */
+          letter-spacing: -0.05em;
+        }
+
+        li {
+          margin-bottom: 0.5em;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+          margin-bottom: 0.5rem;
+          margin-top: 1rem;
+        }
+
+        p {
+          text-indent: 20px;
+        }
+
+        h3 {
+          margin-top: 0.25rem;
+          margin-bottom: 0;
+          font-size: 14px;
+        }
+        h1 {
+          font-family: IBM Plex Serif;
+          font-style: normal;
+          font-weight: 300;
+          font-size: 32px;
+          line-height: 94%;
+          margin: 0;
+          letter-spacing: -0.065em;
+        }
+        h2 {
+          font-family: Inter;
+          font-style: italic;
+          font-weight: 1000;
+          font-size: 1.25rem;
+          margin: 0.25rem;
+          border-bottom: 1px dotted black;
+        }
+        .footer {
+          display: flex;
+          align-items: center;
+        }
+        .deck {
+          height: 473px;
+          width: 442px;
+          margin-bottom: -50px;
+          background-repeat: no-repeat;
+          background-size: 100% 100%;
+          background-image: linear-gradient(
+              to bottom,
+              rgba(255, 255, 255, 0) 70%,
+              rgba(255, 255, 255, 1) 90%
+            ),
+            url(/cyberdeck.png);
+        }
+      `}</style>
+    </ResumeLayout>
+  );
+};
+
+export default Pay;
